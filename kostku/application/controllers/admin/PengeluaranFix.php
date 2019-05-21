d9205e280706110322acb998c059973e2594e943
aliffi98 <jakw788@gmail.com>
2019-05-21 13:12:09 +0700

    Pengeluaran Fix

---
+++ kostku/application/controllers/admin/PengeluaranFix.php
@@ -0,0 +1,40 @@
+<?php 
+
+class PengeluaranFix extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('M_pfix');
+                $this->load->helper('url');
+	}
+
+	function index(){
+		$data['pengeluaran_fix'] = $this->M_pfix->tampil_data()->result();
+		$this->load->view('admin/pengeluaranfix/v_pengeluaran',$data);
+	}
+	
+	function tambah_aksi(){
+		$nama_pengeluaran = $this->input->post('nama_pengeluaran');
+		$biaya = $this->input->post('biaya');
+		
+		$data = array(
+			'nama_pengeluaran' => $nama_pengeluaran,
+			'biaya' => $biaya
+			);
+		
+		$this->M_pfix->input_data($data,'pengeluaran_fix');
+		redirect('admin/PengeluaranFix/index');
+	}
+	
+	function edit($id){
+	$where = array('fix_id' => $id);
+	$data['pengeluaran_fix'] = $this->M_pfix->edit_data($where,'pengeluaran_fix')->result();
+	$this->load->view('admin/pengeluaranfix/v_edit_pengeluaran',$data);
+	}
+	
+	function hapus($id){
+		$where = array('fix_id' => $id);
+		$this->M_pfix->hapus_data($where,'pengeluaran_fix');
+		redirect('admin/pengeluaranfix/index');
+	}
+}
