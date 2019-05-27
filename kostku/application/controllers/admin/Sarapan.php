8adfa7aa971aa239cd76c2cd6145c40551c7a086
hasbi908 <lbikang908@gmail.com>
2019-05-27 18:14:47 +0700

    Menu Sarapan

--- kostku/application/controllers/admin/Sarapan.php
+++ kostku/application/controllers/admin/Sarapan.php
@@ -1,10 +1,54 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php 
 
-class Sarapan extends CI_Controller {
+class Sarapan extends CI_Controller{
 
-	public function index()
-	{
-		$this->load->view('admin/sarapan/v_menu');
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_menu_sarapan');
+        $this->load->helper('url');
+	}
+
+	function index(){
+		$data['menu_sarapan'] = $this->m_menu_sarapan->tampil_data()->result();
+		$this->load->view('admin/sarapan/v_menu',$data);
+	}
+	
+	function tambah_aksi(){
+		$nama_menu = $this->input->post('nama_menu');
+		$harga = $this->input->post('harga');
+		$data = array(
+			'nama_menu' => $nama_menu,
+			'harga' => $harga
+			);
+		$this->m_menu_sarapan->input_data($data,'menu_sarapan');
+		redirect('admin/sarapan/index');
+	}
+	
+	function edit($id){
+	$where = array('menu_sarapan_id' => $id);
+	$data['menu_sarapan'] = $this->m_menu_sarapan->edit_data($where,'menu_sarapan')->result();
+	$this->load->view('admin/sarapan/v_edit_menu',$data);
+	}
+
+	function update(){
+	$biaya_kost_id = $this->input->post('menu_sarapan_id');
+	$nama_pengaturan = $this->input->post('nama_menu');
+	
+	$data = array(
+		'nama_menu' => $nama_menu
+	);
+
+	$where = array(
+		'menu_sarapan_id' => $menu_sarapan_id
+	);
+
+	$this->m_menu_sarapan->update_data($where,$data,'menu_sarapan');
+	redirect('admin/sarapan/index');
+}
+	
+	function hapus($id){
+		$where = array('menu_sarapan_id' => $id);
+		$this->m_menu_sarapan->hapus_data($where,'menu_sarapan');
+		redirect('admin/sarapan/index');
 	}
 }
--- kostku/application/views/admin/sarapan/v_edit_menu.php
+++ kostku/application/views/admin/sarapan/v_edit_menu.php
@@ -21,28 +21,6 @@
 			<!-- Content area -->
 			<div class="content">
 				<!-- Basic card -->
-				<div class="row">
-	<div class="col-md-12">
-		<div class="card">
-			<div class="card-header header-elements-inline">
-				<h6 class="card-title">Menu Sarapan Kost</h6>
-
-			</div>
-
-			<div class="card-body">
-				<ul class="nav nav-tabs nav-tabs-highlight">
-					<li class="nav-item"><a href="#" class="nav-link active"><i class="icon-book mr-2"></i> Daftar Menu</a></li>
-					<li class="nav-item"><a href="<?php echo base_url('admin/pesan_sarapan'); ?>" class="nav-link"  "><i class="icon-list-ordered mr-2"></i> Daftar Pesanan</a></li></li>
-
-				</ul>
-
-				<div class="tab-content">
-					<div class="row">
-	<div class="col-md-12">
-		<div class="card">
-			<div class="card-body">
-								
-				<!-- Basic card -->
 					<div class="row">
 		<div class="col-md-6">
 				<div class="card">
@@ -66,23 +44,24 @@
 							</tr>
 						</thead>
 						<tbody>
-						<?php 
-						$no = 1;
-						foreach($menu_sarapan as $u){ 
-						?>
-						<tr>
-							<td><?php echo $no++ ?></td>
-							<td><?php echo $u->nama_menu ?></td>
-							<td><?php echo $u->harga ?></td>
-							<td>
-								<?php echo anchor('admin/sarapan/edit/'.$u->menu_sarapan_id,'<button class="btn btn-primary"><i class="icon-pencil7"></i></button>'); ?>
+															<tr>
+		<?php 
+		$no = 1;
+		foreach($menu_sarapan as $u){ 
+		?>
+		<tr>
+			<td><?php echo $no++ ?></td>
+			<td><?php echo $u->nama_menu ?></td>
+			<td><?php echo $u->harga ?></td>
+			<td>
+				<?php echo anchor('admin/Sarapan/edit/'.$u->menu_sarapan_id,'<button class="btn btn-primary"><i class="icon-pencil7"></i></button>'); ?>
 
-								<?php //echo anchor('admin/lantai/hapus/'.$u->data_lantai_id,'<button class="btn btn-danger"><i class="icon-trash"></i></button>'); ?>
-								
-								
-							</td>
-						</tr>
-						<?php } ?>
+				<?php echo anchor('admin/Sarapan/hapus/'.$u->menu_sarapan_id,'<button class="btn btn-danger"><i class="icon-trash"></i></button>'); ?>
+				
+				
+			</td>
+		</tr>
+		<?php } ?>
 									
 														
 						</tbody>
@@ -99,22 +78,22 @@
 				<div class="card">
 					<ul class="nav nav-tabs nav-tabs-highlight mb-0">
 						<li class="nav-item"><a href="#bordered-tab1" class="nav-link active" data-toggle="tab">
-							Form Menu</a>
-						</li>
+						Edit Menu</a></li>
 					</ul>
 					<div class="tab-content card card-body border-top-0 rounded-top-0 mb-0">
 					<div class="tab-pane fade show active" id="bordered-tab1">				
 					<div class="card-body">
-									<form action="<?php echo base_url(). 'admin/Sarapan/tambah_aksi'; ?>" method="post">
+									<form action="<?php echo base_url(). 'admin/Sarapan/update'; ?>" method="post">
 									<fieldset>
 										<legend class="font-weight-semibold text-uppercase font-size-sm">
-											<i class="icon-file-plus mr-2"></i>
-											Tambah Menu
+											<i class="icon-reading mr-2"></i>
+											Menu Form
 											<a class="float-right text-default" data-toggle="collapse" data-target="#demo2">
 												<i class="icon-circle-down2"></i>
 											</a>
 										</legend>
 
+												<input type="hidden" name="id" value="<?php echo $u->menu_sarapan_id ?>">
 										<div class="collapse show" id="demo1">
 											<div class="form-group">
 												<label>Nama Menu:</label>
@@ -122,20 +101,15 @@
 													type="text" 
 													name="nama_menu" 
 													class="form-control" 
-													placeholder="Nama Menu">
-											</div>
-											<div class="form-group">
-												<label>Harga :</label>
-												<input 
-													type="number" 
-													name="harga" 
-													class="form-control" 
-													placeholder="Harga menu">
+													placeholder="Nama Menu Sarapan"
+													value="<?php echo $u->nama_menu ?>">
 											</div>
 										</div>
 									</fieldset>
 									<div class="text-right">
-										  <button type="Submit" class="btn btn-primary">Tambah<i class="icon-paperplane ml-2"></i></button>
+										 <button type="submit" class="btn btn-primary">Update<i class="icon-paperplane ml-2"></i></button>
+
+										 <a href=""><button type="button" class="btn btn-default">Cancel <i class="icon-arrow-left8 ml-2"></i></button></a>
 									</div>
 								</form>
 							</div>
@@ -150,29 +124,8 @@
 
 		</div>
 	</div>
-	</div>				
-					</div>
-					</div>
-					</div>
-					</div>
-
-					
-
-				</div>
-			</div>
-		</div>
-
-		<!-- /action toolbar -->
-
-
-
-
-
-	</div>
-
-</div>
-
-
+	
+        
 			</div>
 			<!-- /content area -->
 		</div>
@@ -180,12 +133,25 @@
 	</div>
 	<!-- /page content -->
 	<!-- Footer -->
-    <?php $this->load->view("admin/part/footer.php") ?>
-		
+	<?php $this->load->view("admin/part/footer.php") ?>
 	<!-- /footer -->
-		<!-- Theme JS files -->
-	<?php $this->load->view("admin/part/js.php") ?>
+			<!-- Theme JS files -->
+			<?php $this->load->view("admin/part/js.php") ?>
+	<script type="text/javascript">
 	
+		    $('#user-lists').DataTable({
+		    	autoWidth: false,
+		    	columnDefs: [
+				{ width: 5, targets: 0 },
+				{ width: 120, targets: 1 },
+				{ width: 100, targets: 2 },
+				
+				
+			],
+            dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>'
+		    });
+	
+	</script>
 
 </body>
 </html>
